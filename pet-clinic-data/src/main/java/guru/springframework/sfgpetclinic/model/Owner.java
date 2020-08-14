7b499ecb30986175f354ce8bf72df962dbe2fbc1
hellcat178 <50086886+hellcat178@users.noreply.github.com>
2020-08-14 10:04:47 -0400

    Added Visit Entity, update owner with pet relationship. Closes #26.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Create by bh on 8/4/2020.
  */
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
